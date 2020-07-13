960a4dff82ca4a2c48e242a926913c192aae1cb7
Felipe Osório <felipe.osorio72@outlook.com.br>
2020-07-13 18:51:42 -0300

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { firestorePlugin } from 'vuefire'
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 Vue.use(firestorePlugin)
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
