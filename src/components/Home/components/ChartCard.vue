f3e67c4767f7c9bcb91ba69548faba6b6948d755
Felipe Osório <felipe.osorio72@outlook.com.br>
2020-07-14 19:22:27 -0300

    Add cards showing chat's name and description on home

---
+++ src/components/Home/components/ChartCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card class="chart-card">
+    <v-card-title>
+      {{ name }}
+    </v-card-title>
+    <v-card-text>
+      {{ description }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn text color="deep-purple accent-4">
+        Enter
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ChartCard',
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.chart-card
+  margin: 0 16px
+  flex: 1 0 calc(33% - 32px)
+</style>
--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    {{ documents }}
+  <div class="home">
+    <chart-card 
+      v-for="(chat, index) in chats"
+      :key="index"
+      v-bind="chat"
+    />
   </div>
 </template>
 
 <script>
-import { db } from '~db'
+import { db } from "~db";
+import ChartCard from "./components/ChartCard";
 
 export default {
-  name: 'Home',
-  data: () => ({ documents: [] }),
-  firestore: { documents: db.collection('documents') },
-}
+  name: "Home",
+  components: { ChartCard },
+  data: () => ({ chats: [] }),
+  firestore: { chats: db.collection("chats") },
+};
 </script>
+
+<style lang="sass" scoped>
+.home
+  display: flex
+</style>
